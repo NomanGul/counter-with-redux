4f1603d188580f18e6b58f4fb8fb8c712f4a8f32
NomanGul <nomangul2001@gmail.com>
2019-01-20 00:09:30 +0500

    Updates

---
+++ precache-manifest.daf3cd002ce8a35b811d967df271c775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ff855f5849ae25b6e2b",
+    "url": "/counter-with-redux/static/css/main.09c4217c.chunk.css"
+  },
+  {
+    "revision": "7ff855f5849ae25b6e2b",
+    "url": "/counter-with-redux/static/js/main.7ff855f5.chunk.js"
+  },
+  {
+    "revision": "bbd30344b16e079a18ac",
+    "url": "/counter-with-redux/static/js/1.bbd30344.chunk.js"
+  },
+  {
+    "revision": "cdf4fb64ed76c0e8153f",
+    "url": "/counter-with-redux/static/js/runtime~main.cdf4fb64.js"
+  },
+  {
+    "revision": "e29d10466012cab2c1862b880cd28482",
+    "url": "/counter-with-redux/index.html"
+  }
+];
