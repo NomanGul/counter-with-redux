0c8db263dae0db3b954780e2f477837f39527117
NomanGul <nomangul2001@gmail.com>
2019-01-19 23:41:35 +0500

    redux state added and react state removed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from "react-redux";
 
 class App extends Component {
-  state = {
-    counter: 0
-  };
+  // state = {
+  //   count: 0
+  // };
 
-  increaseCount = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  };
+  // increaseCount = () => {
+  //   const { count } = this.state;
+  //   this.setState({ count: count + 1 });
+  // };
 
-  decreaseCount = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter - 1 });
-  };
+  // decreaseCount = () => {
+  //   const { count } = this.state;
+  //   this.setState({ count: count - 1 });
+  // };
 
   render() {
     return (
       <div className="App">
-        <p>Count: {this.state.counter}</p>
-        <button onClick={this.increaseCount}>Increase</button>
-        <button onClick={this.decreaseCount}>Decrease</button>
+        <p>Count: {this.props.count}</p>
+        <button onClick={this.props.increaseCount}>Increase</button>
+        <button onClick={this.props.decreaseCount}>Decrease</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    count: state.count
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCount: () => dispatch({ type: "INCREASE" }),
+    decreaseCount: () => dispatch({ type: "DECREASE" })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
